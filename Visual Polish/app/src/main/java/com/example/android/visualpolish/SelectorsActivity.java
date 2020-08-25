184c80890d9d57270540d6dcfd13f37027f435ee
keatshackon <keatshackon@gmail.com>
2020-08-25 08:45:21 +0530

    Learn about UI Design

---
+++ Visual Polish/app/src/main/java/com/example/android/visualpolish/SelectorsActivity.java
@@ -0,0 +1,51 @@
+package com.example.android.visualpolish;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class SelectorsActivity extends AppCompatActivity
+        implements SelectorItemsAdapter.ListItemClickListener{
+
+    private static final String TAG = SelectorsActivity.class.getSimpleName();
+
+    // recycler view and adapter
+    RecyclerView mRecyclerView;
+    SelectorItemsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selectors_activity);
+
+        // Reference the recycler view with a call to findViewById
+        mRecyclerView = findViewById(R.id.mainRecyclerView);
+
+        // The linear layout manager will position list items in a vertical list
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
+        // The adapter is responsible for displaying each item in the list
+        mAdapter = new SelectorItemsAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
+        // In here, handle what happens when an item is clicked
+        // In this case, I am just logging the index of the item clicked
+
+//        Log.v(TAG, "List item clicked at index: " + clickedItemIndex);
+
+        Toast.makeText(this, ""+clickedItemIndex, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
